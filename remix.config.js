11181a48ebe925309b57d0feba598664c54bb75a
desmadrestudio <apps@desmadrestudio.com>
2025-07-03 02:03:29 -0500

    fix appointments new route

---
+++ remix.config.js
@@ -0,0 +1,11 @@
+export default {
+  ignoredRouteFiles: ["**/.*"],
+  future: {
+    v3_fetcherPersist: true,
+    v3_relativeSplatPath: true,
+    v3_throwAbortReason: true,
+    v3_lazyRouteDiscovery: true,
+    v3_singleFetch: false,
+    v3_routeConvention: true,
+  },
+};
