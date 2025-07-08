a12293a5985f729212f3c51d54bf2894757f7e36
desmadrestudio <apps@desmadrestudio.com>
2025-07-07 19:40:51 -0500

    Fix: Add appointments index route + nested layout

---
+++ app/routes/appointments/index.tsx
@@ -0,0 +1,11 @@
+import { Page, Card } from "@shopify/polaris";
+
+export default function AppointmentsIndex() {
+  return (
+    <Page title="Appointments">
+      <Card sectioned>
+        <p>This is the Appointments landing page. Use the sidebar to navigate further.</p>
+      </Card>
+    </Page>
+  );
+}
